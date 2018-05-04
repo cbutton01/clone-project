e9c05236ae3b07660363a92c1070b2fbbed3614b
cb <csbutton01@hotmail.com>
2018-05-04 16:22:14 -0700

    remove uneeded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { GameComponent } from './game/game.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { NewGameComponent } from './new-game/new-game.component';
 
@@ -11,7 +10,6 @@ import { NewGameComponent } from './new-game/new-game.component';
   declarations: [
     AppComponent,
     ProfileComponent,
-    GameComponent,
     GameListComponent,
     NewGameComponent
   ],
