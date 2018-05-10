8a36ade3114c2015991f8a4f09e210ace1836602
cb <csbutton01@hotmail.com>
2018-05-09 17:54:55 -0700

    add array of generic games

--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -1,5 +1,8 @@
 import { Game } from './models/game.model';
 
 export const GAMES: Game[] = [
-  new Game('Thor', 'Be the thunder god!', 30)
+  new Game('Thor', 'Be the thunder god!', 30),
+  new Game('Explosions', 'Blow things up!', 30),
+  new Game('Gratuitous Violence', 'The Title Says it All!', 30),
+  new Game('Generic FPS', 'Thats right! Another FPS!', 30)
 ];
